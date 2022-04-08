5af7233b6cf1e6d063794d3cd3edaa8ae7eccbf4
Daniel Jones <danieljones028@gmail.com>
2022-04-08 18:52:48 -0300

    Teste Producao 001

--- pysmc.py
+++ pysmc.py
@@ -20,20 +20,26 @@ zabbix_timeout          =   config['DEFAULT']['zabbix_timeout']
 zabbix_template_name    =   config['DEFAULT']['zabbix_template_name']
 zabbix_application_name =   config['DEFAULT']['zabbix_application_name']
 
-
-
 zmprov = config['DEFAULT']['zmprov']
 
 hostname = socket.gethostname()
 current_log = '/var/log/zimbra.log'
 
-# Staging
-# homelog = '/home/daniel_jones/Documentos/Linux/Zabbix/Zimbra/logs/zimbra.log-20220405'
-# pathFileAccounts = '/home/daniel_jones/Documentos/Linux/Zabbix/Zimbra/send_monitoring_counting/temp_test/listaDeContas.txt'
-###############################################################################
-
 # TODO incluir log e criacao automatica de item no zabbix
 
-accounts = SendDataToZabbix.getAccounts(zmprov, hostname, domain)
-SendDataToZabbix.Testeitem(zabbix_server_url, zabbix_user, zabbix_user_password, zabbix_session_verify, zabbix_timeout, zabbix_template_name, zabbix_application_name, 'Maluco', 'Doido')
-SendDataToZabbix.parsingsend(accounts, current_log, domain, zabbixsender, zabbixserver, zabbixclient)
+try:
+    accounts = SendDataToZabbix.getAccounts(zmprov, hostname, domain)
+except Exception as e:
+    print(e)
+
+try:
+    for account in accounts:
+        account = account.replace(domain, '')
+        SendDataToZabbix.createItem(zabbix_server_url, zabbix_user, zabbix_user_password, zabbix_session_verify, zabbix_timeout, zabbix_template_name, zabbix_application_name, f'{account}{domain}', f'msgsender.{account}')
+except Exception as e:
+    print(e)
+
+try:
+    SendDataToZabbix.parsingsend(accounts, current_log, domain, zabbixsender, zabbixserver, zabbixclient)
+except Exception as e:
+    print(e)
